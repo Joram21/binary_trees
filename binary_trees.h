324625988373bc5409536bd8fa021e079d002e78
Joram21 <oloojoram@gmail.com>
2023-08-30 09:45:20 -0700

    update

--- binary_trees.h
+++ binary_trees.h
@@ -34,6 +34,8 @@ int binary_tree_is_root(const binary_tree_t *node);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 /* SIZE_T FUNCT */
 size_t binary_tree_size(const binary_tree_t *tree);
